722f294040a11ff327177a6ed09b768cddf9a092
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-12 11:56:48 -0300

    feat: implement modalComp

---
+++ src/components/ModalComp.jsx
@@ -0,0 +1,82 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+import React from "react";
+
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+  const [name, setName] = useState(dataEdit.name || "");
+  const [email, setEmail] = useState(dataEdit.email || "");
+
+  const handleSave = () => {
+    if (!name || !email) return;
+    if (emailAlreadyExists()) {
+      return alert("Email já cadastrado!");
+    }
+
+    if (Object.keys(dataEdit).length) {
+      data[dataEdit.index] = { name, email };
+    }
+
+    const newDataArray = !Object.keys(dataEdit).length
+      ? [...(data ? data : []), { name, email }]
+      : [...(data ? data : [])];
+
+    localStorage.setItem("cad_cliente", JSON.stringify(newDataArray));
+
+    setData(newDataArray);
+    onClose();
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Cadastro de usuário</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl dispay="flex" flexDir="column" gap={4}>
+            <Box>
+              <FormLabel htmlFor="name">Nome</FormLabel>
+              <Input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <FormLabel htmlFor="email">Nome</FormLabel>
+              <Input
+                type="text"
+                value={email}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Box>
+          </FormControl>
+        </ModalBody>
+
+        <ModalFooter justifyContent="start">
+          <Button colorScheme="green" mr={3} onClick={handleSave}>
+            SALVAR
+          </Button>
+          <Button colorScheme="red" onClick={onClose}>
+            CANCELAR
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalComp;
